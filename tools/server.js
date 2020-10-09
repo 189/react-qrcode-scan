301bf451f8f70b97a4256a1fb43e75209ca7e7d2
189 <kv.wang87@gmail.com>
2020-10-09 11:31:44 +0800

    auto open when dev server is ok

--- tools/server.js
+++ tools/server.js
@@ -1,17 +1,19 @@
 const Webpack = require("webpack");
 const WebpackDevServer = require("webpack-dev-server");
 const webpackConfig = require("../conf/webpack.example");
+const open = require("open");
 
 const devPort = 8898;
 const compiler = Webpack(webpackConfig);
 const devServerOptions = Object.assign({}, webpackConfig.devServer, {
 	open: true,
 	stats: {
-		colors: true
-	}
+		colors: true,
+	},
 });
 const server = new WebpackDevServer(compiler, devServerOptions);
 
 server.listen(devPort, "127.0.0.1", () => {
 	console.log("Starting server on http://localhost:" + devPort);
+	open("http://localhost:" + devPort);
 });
